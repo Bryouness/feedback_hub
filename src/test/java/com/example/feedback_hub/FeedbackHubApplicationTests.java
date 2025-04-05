648a9a7c8f0f05711be22ba23d31f394f2ec76bb
Youness Bourhim <bourhimyouness4@gmail.com>
2025-04-05 12:59:19 +0000

    connected to database feedback_db

---
+++ src/test/java/com/example/feedback_hub/FeedbackHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feedback_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
